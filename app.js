1f8f4fd56765d61ffc78367ca8122dac03ff3b40
SainathBangera1 <psainathbangera@gmail.com>
2022-01-06 16:36:21 +0530

    fifth commit

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-//const {v4:uuid} = require('uuid'); 
-//console.log(uuid());
-console.log('oR Alternative');
-//cant use both at same time
-const uuid = require('uuid');
-console.log(uuid.v4());
+const logEvents = require('./utilities/C-logEvents');
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {};
+
+//Intialise object
+const myEmitter = new MyEmitter();
+
+//add listner for the log event
+myEmitter.on('log', (msg)=>logEvents(msg));
+
+setTimeout(()=>{
+    //Emit Event
+    myEmitter.emit('log','log event emitted!!!');
+},2000);
---
+++ utilities/B-uuidModules.js
@@ -0,0 +1,6 @@
+//const {v4:uuid} = require('uuid'); 
+//console.log(uuid());
+console.log('oR Alternative');
+//cant use both at same time
+const uuid = require('uuid');
+console.log(uuid.v4());
